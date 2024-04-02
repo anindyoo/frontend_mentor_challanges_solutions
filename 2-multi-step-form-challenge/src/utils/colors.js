01a5d31288d1dcd4c764471b3cb259d0080caa7c
anindyoo <anindyo0216@gmail.com>
2024-04-02 10:14:48 +0700

    feat: create colors object.

---
+++ 2-multi-step-form-challenge/src/utils/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+  // primary
+  marineBlue: '#02295a',
+  purplishBlue: '#473dff',
+  pastelBlue: '#adbeff',
+  lightBlue: '#bfe2fd',
+  strawberryRed: '#ed3548',
+  
+  // neutral
+  coolGray: '#9699ab',
+  lightGray: '#d6d9e6',
+  magnolia: '#f0f6ff',
+  alabaster: '#fafbff',
+  white: '#ffffff',
+};
+
+export default colors;
