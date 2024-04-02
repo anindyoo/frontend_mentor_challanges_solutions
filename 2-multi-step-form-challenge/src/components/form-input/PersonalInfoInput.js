b857b5feb31aed10dd36293e743a0d29ffc85f00
anindyoo <anindyo0216@gmail.com>
2024-04-02 10:40:18 +0700

    style: remove whitespace.

--- 2-multi-step-form-challenge/src/components/form-input/PersonalInfoInput.js
+++ 2-multi-step-form-challenge/src/components/form-input/PersonalInfoInput.js
@@ -41,7 +41,6 @@ export default function PersonalInfoInput(props) {
         [eventName]: eventValue,
       }
     });
-    
 
     const formatValidation = (name, value) => {
       switch(name) {
