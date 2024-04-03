68e6d45f829d96e7eaf481fd40d047bd550004ee
anindyoo <anindyo0216@gmail.com>
2024-04-03 21:43:20 +0700

    refactor: remove comments.

--- 2-multi-step-form-challenge/src/components/form-context/FormContext.js
+++ 2-multi-step-form-challenge/src/components/form-context/FormContext.js
@@ -4,20 +4,16 @@ import FormInput from '../form-input/FormInput';
 import FormSidebar from '../form-sidebar/FormSidebar';
 
 export const StepContext = createContext();
-// export const PersonalInfoContext = createContext();
 
 export default function FormContext() {
   const [step, setStep] = useState(0);
-  // const [personalInfoData, setPersonalInfoData] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('mhmmm mhmhmhmhhm')
   }
   
   return (
     <StepContext.Provider value={{ step, setStep }}>
-      {/* <PersonalInfoContext.Provider value={{ personalInfoData, setPersonalInfoData }}> */}
         <form action="" onSubmit={handleSubmit} encType='multipart/form-data'>
           <section 
             className="flex justify-between min-w-[58.75rem] p-4 rounded-2xl drop-shadow-[0_25px_25px_rgba(0,0,0,0.0477)]"
@@ -26,7 +22,6 @@ export default function FormContext() {
               <FormInput />
           </section>
         </form>  
-      {/* </PersonalInfoContext.Provider> */}
     </StepContext.Provider>
   );
 }
