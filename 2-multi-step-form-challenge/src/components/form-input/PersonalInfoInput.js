06d9896d0d81243ac93dfe1b174b6dc19d2abc55
anindyoo <anindyo0216@gmail.com>
2024-04-05 15:10:39 +0700

    feat: migrate personal info input data from state management to context management.

--- 2-multi-step-form-challenge/src/components/form-input/PersonalInfoInput.js
+++ 2-multi-step-form-challenge/src/components/form-input/PersonalInfoInput.js
@@ -1,45 +1,36 @@
-import { useEffect } from "react";
-import colors from "../../utils/colors";
+import { useContext } from "react";
+import { GlobalInputContext } from "../form-context/FormContext";
+
+export const labelDetails = [
+  {
+    id: 'name',
+    label: 'Name',
+    type: 'text',
+    placeholder: 'e.g. Stephen King',
+  },
+  {
+    id: 'email',
+    label: 'Email Address',
+    type: 'email',
+    placeholder: 'e.g. stephenking@lorem.com',
+  },
+  {
+    id: 'phone',
+    label: 'Phone Number',
+    type: 'tel',
+    placeholder: 'e.g. +1 234 567 890',
+  },
+];
 
 export default function PersonalInfoInput(props) {
-  const { 
-    personalInfoData,
-    setPersonalInfoData,   
-    inputsError,
-    setInputsError, 
-    nextButtonIsClicked} = props;
+  const {nextButtonIsClicked} = props;
 
-  const labelDetails = [
-    {
-      id: 'name',
-      label: 'Name',
-      type: 'text',
-      placeholder: 'e.g. Stephen King',
-    },
-    {
-      id: 'email',
-      label: 'Email Address',
-      type: 'email',
-      placeholder: 'e.g. stephenking@lorem.com',
-    },
-    {
-      id: 'phone',
-      label: 'Phone Number',
-      type: 'tel',
-      placeholder: 'e.g. +1 234 567 890',
-    },
-  ]
+  const globalInputContext = useContext(GlobalInputContext).globalInputState;
+  const setGlobalInputContext = useContext(GlobalInputContext).setglobalInputState;
 
   const handleInputChange = (event) => {
     let eventName = event.target.name;
     let eventValue = event.target.value;
-    
-    setPersonalInfoData(previousValue => {
-      return {
-        ...previousValue,
-        [eventName]: eventValue,
-      }
-    });
 
     const formatValidation = (name, value) => {
       switch(name) {
@@ -53,36 +44,27 @@ export default function PersonalInfoInput(props) {
         default:
       }
     }
-
-    setInputsError(previousError => {
-      const formatError = formatValidation(eventName, eventValue);
-      return {
-        ...previousError,
-        [eventName]: 
-        eventValue === '' ? 'This field is required' 
-        : formatError ? formatError 
-        : '',
-      }
-    });
-
-    event.target.style.borderColor = nextButtonIsClicked && inputsError[eventName] ? colors.strawberryRed : colors.marineBlue;
+    
+    const formatError = formatValidation(eventName, eventValue);
+    
+    setGlobalInputContext((prevInputType) => ({
+      ...prevInputType,
+      personalInfo: {
+        ...prevInputType.personalInfo,
+        inputs: {
+          ...prevInputType.personalInfo.inputs,
+          [eventName]: eventValue,
+        },
+        errors: {
+          ...prevInputType.personalInfo.errors,
+          [eventName]: 
+              eventValue === '' ? 'This field is required' 
+              : formatError ? formatError 
+              : '',
+        }
+      },
+    }));
   }
-
-  useEffect(() => {
-    if (Object.keys(personalInfoData).length === 0){
-      const initialErorrData = labelDetails.reduce((acc, current) => {
-        return { ...acc, [current.id]: 'This field is required' };
-      }, {});
-      
-      const initialPersonalInfoData = labelDetails.reduce((acc, current) => {
-        return { ...acc, [current.id]: '' };
-      }, {});
-      
-      setInputsError(initialErorrData);
-      setPersonalInfoData(initialPersonalInfoData);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   
   return (
     <section className='flex flex-col gap-5'>
@@ -90,28 +72,24 @@ export default function PersonalInfoInput(props) {
         <div key={`label-` + index}>
           <div className="flex justify-between mb-1.5">
             <label 
-              htmlFor="name" 
-              className="text-sm font-regular "
-              style={{ color: colors.marineBlue }}>{label.label}</label>
+              htmlFor={label.id}
+              className="text-sm font-regular text-marineBlue"
+            >{label.label}</label>
             <p 
-            className="text-sm font-bold"
-            style={{ color:colors.strawberryRed }}>{nextButtonIsClicked && inputsError[label.id]}</p>
+              className="text-sm font-bold text-strawberryRed"
+            >{nextButtonIsClicked && globalInputContext.personalInfo.errors[label.id]}</p>
           </div>          
           <input 
             type={label.type} 
             id={label.id} 
             name={label.id}
-            value={Object.keys(personalInfoData).length !== 0 && personalInfoData[label.id]}
+            value={globalInputContext.personalInfo.inputs[label.id]}
             onChange={handleInputChange}
-            className="border w-full min-h-12 px-4 rounded-md font-medium focus:outline-none" 
             placeholder={label.placeholder}
-            style={{ 
-              borderColor: nextButtonIsClicked && inputsError[label.id] ? colors.strawberryRed : colors.lightGray,
-              color: colors.marineBlue, 
-            }}          
-            onFocus={(event) => event.target.style.borderColor = nextButtonIsClicked && inputsError[label.id] ? colors.strawberryRed : colors.marineBlue} 
-            onBlur={(event) => event.target.style.borderColor = nextButtonIsClicked && inputsError[label.id] ? colors.strawberryRed : colors.lightGray}
-            />
+            className={`border w-full min-h-12 px-4 rounded-md font-medium text-marineBlue focus:outline-none border-lightGray ${
+              nextButtonIsClicked && globalInputContext.personalInfo.errors[label.id] ?  `border-strawberryRed` : `border-marineBlue`
+            }`}
+            required />
         </div>
       ))}
     </section>
