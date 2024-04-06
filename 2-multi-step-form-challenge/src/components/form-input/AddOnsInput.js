725cc1947bd70ebe0af829b571f871f34215013a
anindyoo <anindyo0216@gmail.com>
2024-04-06 16:18:29 +0700

    feat: migrate add-ons  input data from state management to context management.

--- 2-multi-step-form-challenge/src/components/form-input/AddOnsInput.js
+++ 2-multi-step-form-challenge/src/components/form-input/AddOnsInput.js
@@ -1,50 +1,72 @@
+import { useContext } from "react";
 import colors from "../../utils/colors"
+import { GlobalInputContext } from "../form-context/FormContext";
 
 export const addOnData = [
   {
+    id:'onlineService',
     name: "Online service",
     description: "Access to multiplayer games",
-    price: 1,
+    price: {
+      monthly: 1,
+      yearly: 10,
+    },
   },
   {
+    id:'largerStorage',
     name: "Larger storage",
     description: "Extra 1TB of cloud save",
-    price: 2,
+    price: {
+      monthly: 2,
+      yearly: 20,
+    },
   },
   {
+    id:'customProfile',
     name: "Customizable Profile",
     description: "Custom theme on your profile",
-    price: 2,
+    price: {
+      monthly: 2,
+      yearly: 20,
+    },
   },
 ];
 
-export default function AddOnsInput(props) {
-  const { addOnState, setAddOnState } = props;
+export default function AddOnsInput() {
+  const planContext = useContext(GlobalInputContext).globalInputState.plan;
+  const addOnsContext = useContext(GlobalInputContext).globalInputState.addOns;
+  const setGlobalInputContext = useContext(GlobalInputContext).setglobalInputState;
+ 
+  const handleCheckboxOnChange = (event) => {
+    let selectedAddOnId = event.target.value;
 
-  const handleCheckboxOnChange = (position) => {
-    const updatedAddOnState = addOnState.map((addOn, index) =>
-      position === index ? !addOn : addOn
-    );
-
-    setAddOnState(updatedAddOnState);
+    setGlobalInputContext((prevInput) => ({
+      ...prevInput,
+      addOns:  {
+        ...prevInput.addOns, 
+        [selectedAddOnId]: !addOnsContext[selectedAddOnId],
+      } 
+    }));    
   }
 
   return (
     <section className="flex flex-col gap-4">    
       {addOnData.map((addOn, index) => (
         <label 
-          htmlFor={addOn.name}
+          htmlFor={addOn.id}
           key={`checkbox-` + index}
-          className={`border flex items-center w-full justify-between px-6 py-5 rounded-md ${(addOnState[index] ? `bg-alabaster border-purplishBlue` : `bg-white border-lightGray`)}
-          `}            
+          className={`border flex items-center w-full justify-between px-6 py-5 rounded-md hover:cursor-pointer ${
+            (addOnsContext[addOn.id] ? `bg-alabaster border-purplishBlue` : `bg-white border-lightGray`)
+          }`}            
         >
           <div className="flex items-center">
             <input 
               type="checkbox" 
-              id={addOn.name} 
+              id={addOn.id} 
               className="relative peer shrink-0 appearance-none w-5 h-5 border rounded mt-1 border-lightGray checked:bg-purplishBlue checked:border-0" 
-              checked={addOnState[index]}
-              onChange={() => handleCheckboxOnChange(index)} 
+              checked={addOnsContext[addOn.id]}
+              value={addOn.id}
+              onChange={handleCheckboxOnChange} 
             />        
             <svg
               className="absolute h-3.5 w-3.5 mt-1.5 ml-1 hidden peer-checked:block"
@@ -66,7 +88,9 @@ export default function AddOnsInput(props) {
              <p className="text-[0.938rem] text-coolGray">{addOn.description}</p>
             </div>
           </div>
-          <p className="text-[0.938rem] text-purplishBlue">${addOn.price}/mo</p>
+          <p className="text-[0.938rem] text-purplishBlue">
+            ${planContext.yearlyToggle ? addOn.price.yearly : addOn.price.monthly}/mo
+          </p>
         </label>
       ))}
     </section>
